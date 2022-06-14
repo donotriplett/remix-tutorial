f8014b9b4da86e61f9ebc3654378114719c8757b
Donovan <donotriplett@gmail.com>
2022-06-14 09:44:06 -0400

    finished tutorial

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,11 +1,36 @@
-export default function JokesIndexRoute() {
-    return (
-      <div>
-        <p>Here's a random joke:</p>
-        <p>
-          I was wondering why the frisbee was getting bigger,
-          then it hit me.
-        </p>
+import type {
+  LinksFunction,
+  MetaFunction,
+} from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+import stylesUrl from "~/styles/index.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
+export const meta: MetaFunction = () => ({
+  title: "Remix: So great, it's funny!",
+  description:
+    "Remix jokes app. Learn Remix and laugh at the same time!",
+});
+
+export default function Index() {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-    );
-  }
+    </div>
+  );
+}
